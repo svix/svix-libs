258ef6e814ecdaf3065b629421c80e93dad903db
Mendy Man <mman@svix.com>
2025-02-13 10:55:34 -0500

    kotlin: Add `messageInRaw`

--- kotlin/lib/src/main/kotlin/Message.kt
+++ kotlin/lib/src/main/kotlin/Message.kt
@@ -112,3 +112,49 @@ class Message(private val client: SvixHttpClient) {
         client.executeRequest<Any, Boolean>("DELETE", url.build())
     }
 }
+
+/**
+ * Creates a [MessageIn] with a pre-serialized payload.
+ *
+ * The payload is not normalized on the server. Normally, payloads are required to be JSON, and Svix
+ * will minify the payload before sending the webhooks (for example, by removing extraneous
+ * whitespace or unnecessarily escaped characters in strings). With this function, the payload will
+ * be sent "as is", without any minification or other processing.
+ *
+ * @param payload Serialized message payload
+ * @param contentType The value to use for the Content-Type header of the webhook sent by Svix,
+ *   overwriting the default of `application/json` if specified
+ *
+ * See the class documentation for details about the other parameters.
+ */
+fun messageInRaw(
+    eventType: String,
+    payload: String,
+    contentType: String? = null,
+    application: ApplicationIn? = null,
+    channels: Set<String>? = null,
+    eventId: String? = null,
+    payloadRetentionHours: Long? = null,
+    payloadRetentionPeriod: Long? = 90L,
+    tags: Set<String>? = null,
+    transformationsParams: Map<String, Any> = mapOf(),
+): MessageIn {
+    val transformationsParams = transformationsParams.toMutableMap()
+    transformationsParams.put("rawPayload", payload)
+    if (contentType != null) {
+        val headers = mapOf("content-type" to contentType)
+        transformationsParams.put("headers", headers)
+    }
+
+    return MessageIn(
+        eventType = eventType,
+        payload = mapOf<String, String>(),
+        application = application,
+        channels = channels,
+        eventId = eventId,
+        payloadRetentionHours = payloadRetentionHours,
+        payloadRetentionPeriod = payloadRetentionPeriod,
+        tags = tags,
+        transformationsParams = transformationsParams,
+    )
+}
